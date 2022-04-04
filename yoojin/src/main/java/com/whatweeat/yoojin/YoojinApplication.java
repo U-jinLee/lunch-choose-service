299112a476edd7c3f1be1cea235b050cf15b5558
yoojin Lee <leeujin1029@naver.com>
2022-04-04 13:46:00 +0900

    Feat: Configure essential spring setting

---
+++ yoojin/src/main/java/com/whatweeat/yoojin/YoojinApplication.java
@@ -0,0 +1,13 @@
+package com.whatweeat.yoojin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoojinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoojinApplication.class, args);
+	}
+
+}
---
+++ yoojin/src/test/java/com/whatweeat/yoojin/YoojinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatweeat.yoojin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoojinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
