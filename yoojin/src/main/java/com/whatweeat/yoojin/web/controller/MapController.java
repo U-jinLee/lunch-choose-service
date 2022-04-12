987c2776007c1f5e0a15f0826ff678586b2fc6b7
yoojin Lee <leeujin1029@naver.com>
2022-04-12 16:39:09 +0900

    Feat: Add find restuarant

--- yoojin/src/main/java/com/whatweeat/yoojin/web/controller/MapController.java
+++ yoojin/src/main/java/com/whatweeat/yoojin/web/controller/MapController.java
@@ -1,13 +1,20 @@
 package com.whatweeat.yoojin.web.controller;
 
+import com.whatweeat.yoojin.config.auth.PrincipalDetails;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class MapController {
 
     @GetMapping("/map")
-    public String map() {
+    public String map(Authentication authentication, @AuthenticationPrincipal PrincipalDetails userDetails) {
+        log.info("Authentication principal Info:{}", authentication.getPrincipal().toString());
+        log.info("userDetailsPrincipal annotation: {}", userDetails.getUsername());
         return "map";
     }
 
